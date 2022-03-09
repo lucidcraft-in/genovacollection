713fa299c0de7c1212afedc9ab4e1eac8444747b
salman-threestops <salman.faris@threestops.com>
2022-03-09 16:53:42 +0530

    change path

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, './uploads/')
+    cb(null, '/home/ubuntu/genovacollection//uploads/')
   },
   filename(req, file, cb) {
     cb(
