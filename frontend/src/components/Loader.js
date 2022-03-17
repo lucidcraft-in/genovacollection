5e81b974461ee5ec1429f03d078e386b28806de7
salman-threestops <salman.faris@threestops.com>
2022-03-17 11:33:31 +0530

    loader

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -4,18 +4,19 @@ import { Spinner } from 'react-bootstrap'
 const Loader = () => {
   return (
     <Spinner
-      animation='border'
-      role='status'
+      animation="border"
+      role="status"
+      variant="danger"
       style={{
-        width: '100px',
-        height: '100px',
+        width: '50px',
+        height: '50px',
         margin: 'auto',
         display: 'block',
       }}
     >
-      <span className='sr-only'>Loading...</span>
+      <span className="sr-only">Loading...</span>
     </Spinner>
-  )
+  );
 }
 
 export default Loader
