b57aa18f3b3cbc9d84cd07e2ba1b7179cd30503c
salman-threestops <salman.faris@threestops.com>
2022-04-23 11:36:23 +0530

    app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -109,6 +109,7 @@ const App = () => {
    const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
   
+  
   const [preLoad, setPreload] = useState(true);
 
     useEffect(() => {
