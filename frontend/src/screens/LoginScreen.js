536028417462a8f9f54511756457375887d1c0c7
salman-threestops <salman.faris@threestops.com>
2022-04-23 14:34:26 +0530

    login style

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -6,6 +6,7 @@ import Message from '../components/Message'
 import Loader from '../components/Loader'
 import FormContainer from '../components/FormContainer'
 import { login } from '../actions/userActions'
+import './Login.css'
 
 const LoginScreen = ({ location, history }) => {
   const [email, setEmail] = useState('')
@@ -30,46 +31,94 @@ const LoginScreen = ({ location, history }) => {
   }
 
   return (
-    <FormContainer>
-      <h1>Sign In</h1>
-      {error && <Message variant='danger'>{error}</Message>}
-      {loading && <Loader />}
-      <Form onSubmit={submitHandler}>
-        <Form.Group controlId='email'>
-          <Form.Label>Email Address</Form.Label>
-          <Form.Control
-            type='email'
-            placeholder='Enter email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
+    // <FormContainer>
+    //   <h1>Sign In</h1>
+    //   {error && <Message variant="danger">{error}</Message>}
+    //   {loading && <Loader />}
+    //   <Form onSubmit={submitHandler}>
+    //     <Form.Group controlId="email">
+    //       <span className="text-style">Email Address</span>
+    //       <Form.Control
+    //         type="email"
+    //         placeholder="Enter email"
+    //         value={email}
+    //         onChange={(e) => setEmail(e.target.value)}
+    //       ></Form.Control>
+    //     </Form.Group>
 
-        <Form.Group controlId='password'>
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='Enter password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
+    //     <Form.Group controlId="password">
+    //       <Form.Label>Password</Form.Label>
+    //       <Form.Control
+    //         type="password"
+    //         placeholder="Enter password"
+    //         value={password}
+    //         onChange={(e) => setPassword(e.target.value)}
+    //       ></Form.Control>
+    //     </Form.Group>
 
-        <Button type='submit' variant='primary'>
-          Sign In
-        </Button>
-      </Form>
+    //     <Button type="submit" variant="primary">
+    //       Sign In
+    //     </Button>
+    //   </Form>
 
-      <Row className='py-3'>
-        <Col>
-          New Customer?{' '}
-          <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
-            Register
-          </Link>
-        </Col>
-      </Row>
-    </FormContainer>
-  )
+    //   <Row className="py-3">
+    //     <Col>
+    //       New Customer?{' '}
+    //       <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
+    //         Register
+    //       </Link>
+    //     </Col>
+    //   </Row>
+    // </FormContainer>
+    <>
+      {' '}
+      {loading ? <Loader /> :
+        <div class="container">
+          <div class="wrapper">
+            <div class="title">
+              <span>Sign In</span>
+            </div>
+            <form onSubmit={submitHandler}>
+              <div class="row">
+                <i class="fas fa-user"></i>
+                <input
+                  type="email"
+                  placeholder="Enter email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div class="row">
+                <i class="fas fa-lock"></i>
+                <input
+                  type="password"
+                  placeholder="Enter password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+              {/* <div class="pass">
+              <a href="#">Forgot password?</a>
+            </div> */}
+              <div class="row button">
+                <input type="submit" value="Login" />
+              </div>
+              <div class="signup-link ">
+                Not a member?{' '}
+                <Link
+                  to={redirect ? `/register?redirect=${redirect}` : '/register'}
+                >
+                  Register
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      }
+    </>
+  );
 }
 
 export default LoginScreen
