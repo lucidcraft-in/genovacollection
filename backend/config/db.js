665c174409f00e0b727c4b4ee6e207d6fd7ea775
SALMAN-FRZ <farisareekkad796@gmail.com>
2023-06-25 18:00:10 +0530

    config

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,10 @@ import mongoose from 'mongoose'
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      'mongodb://genova:123@localhost:27017/genova',
+      'mongodb://admin:admin@localhost:27017/genova',
       // 'mongodb+srv://genova:123@genova.mz1ru.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
       // process.env.MONGO_URI,
+      // 'mongodb://localhost:27017/genova',
       {
         useUnifiedTopology: true,
         useNewUrlParser: true,
@@ -15,6 +16,7 @@ const connectDB = async () => {
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
   } catch (error) {
+    console.log("error")
     console.error(`Error: ${error.message}`.red.underline.bold)
     process.exit(1)
   }
